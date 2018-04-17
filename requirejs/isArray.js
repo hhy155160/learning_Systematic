4158def90d555b6ab44d90d79f53c657c398810f
hhy155160 <1659078806@qq.com>
2018-04-17 23:17:33 +0800

    第二次测试

--- requirejs/isArray.js
+++ requirejs/isArray.js
@@ -1,3 +1,14 @@
 /**
  * Created by Administrator on 2018/4/10.
  */
+define(function () {
+    function isArray(arr) {
+        if(arr.constructor == Array&&arr instanceof Array){
+            return 1;
+        }
+        else{
+            return "不是数组";
+        }
+    }
+    return isArray;
+});
